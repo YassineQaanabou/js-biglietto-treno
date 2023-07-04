53499d0754e45fff16c5abbcfc891ac39b53fc62
YassineQaanabou <qaanabou@gmail.com>
2023-07-04 09:36:09 +0200

    fixed base exercise

--- js/main.js
+++ js/main.js
@@ -6,14 +6,17 @@ let price = km * 0.21;
 
 if ( age  <=18) {
     let finalPrice = price - (price*0.20);
-    console.log(finalPrice);
-} else if ( age >=65) {
+    console.log(finalPrice.toFixed(2));
+    document.getElementById("ticketPrice").innerHTML = finalPrice.toFixed(2);
+    } else if ( age >=65) {
     let finalPrice = price - (price*0.40);
-    console.log(finalPrice);
+    console.log(finalPrice.toFixed(2));
+    document.getElementById("ticketPrice").innerHTML = finalPrice.toFixed(2);
+
 } else {
     let finalPrice = price;
-    console.log(finalPrice);
+    console.log(finalPrice.toFixed(2));
+    document.getElementById("ticketPrice").innerHTML = finalPrice.toFixed(2);
+
 }
-
-
 
